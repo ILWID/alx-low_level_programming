a617aa838bc897c3aebffae25910bdf89ffe733b
ILWID <Obasanyaolufemi@gmail.com>
2022-10-13 13:30:48 -0700

    My first commit on C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main - print the phrase "Programming is like building a multilingual puzzle
- * Return: 0 if sucess.
- */
+*main - main block
+*
+*Return: 0
+*
+*/
 int main(void)
 {
-	put("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
