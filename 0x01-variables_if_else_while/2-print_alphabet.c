b3bfa0be4b9248f6684b7fe4da59626a58231580
ILWID <Obasanyaolufemi@gmail.com>
2022-10-14 09:58:53 -0700

    My first commi on c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - print lower case
+ *
+ * Description: using the putchat function this program print lower case
+ * Return:0
+ */
+int main(void)
+{
+	putchar ("print lower case\n");
+}
+return (0);
+}
