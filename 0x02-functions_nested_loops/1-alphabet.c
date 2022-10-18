651c667961d87bfaaf9d36e0cdd5751dd0238a07
ILWID <Obasanyaolufemi@gmail.com>
2022-10-18 08:12:30 -0700

    My second commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,19 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_alphabet - function to print abc
  *
+ * Description: using the main function and print_alphabet function
+ * this program prints "abcdefghijklmnopqrstuvwxyz
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet(void)
 {
-	print_alphabet();
-	return (0);
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
 }
