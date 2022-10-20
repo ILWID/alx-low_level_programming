8fcf6c69f6017738c5af519968183b024d214b4a
ILWID <Obasanyaolufemi@gmail.com>
2022-10-20 14:33:22 -0700

    test

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that print numbers
+ * @c: numbers from 0 to 9
+ * Description: using the print_number function
+ * this program print "
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	char c, i;
+
+	for (i = 0 ; i <= 9 ; i++)
+		_putchar(c);
+		_putchar('\n');
+}
