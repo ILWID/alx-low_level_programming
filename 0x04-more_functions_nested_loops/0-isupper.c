4339a137279b798b39f30d332ff4921c7462b28f
ILWID <Obasanyaolufemi@gmail.com>
2022-10-20 09:55:59 -0700

    test

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - function that checks for upperharcater
+ * @c: The input character to check
+ * Return: 0 || 1 (success)
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (0);
+	else
+		return (0);
+}
