13851bc9718b22976de83a9652b25b08b49a4cfc
ILWID <Obasanyaolufemi@gmail.com>
2022-10-24 13:24:11 -0700

    Point

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,9 +10,12 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while ((i % 2 == 0) && str[i] != '\0')
+	while (str[i] != '\0')
 	{
-		putchar(str[i]);
+		if (i % 2 == 0)
+		{
+			putchar(str[i]);
+		}
 		i++;
 	}
 	putchar('\n');
