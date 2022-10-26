e2e27aacb2da4229ac42ea7ab880ed8fe66b665e
ILWID <Obasanyaolufemi@gmail.com>
2022-10-26 16:44:55 -0700

    string

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcmp - function that compares two strings.
+ * @s1:first string argument character
+ * @s2: second string argument character
+ *
+ * Return: the compare values
+ */
+int _strcmp(char *s1, char *s2)
+{
+	strcmp(s1, s2);
+	return (strcmp(s1, s2));
+}
