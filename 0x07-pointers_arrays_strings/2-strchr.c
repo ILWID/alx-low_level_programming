bade04de78d4dc7711d73a4dc00c0e2c918624af
ILWID <Obasanyaolufemi@gmail.com>
2022-10-31 08:06:12 -0700

    pointer

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strchr - function that locates a character in a string
+ * @s: string character
+ * @c: character in the string
+ *
+ * Return: nothing
+ */
+char *_strchr(char *s, char c)
+{
+	strchr(s, c);
+	return (0);
+}
