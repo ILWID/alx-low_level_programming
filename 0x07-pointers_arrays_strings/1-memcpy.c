08b044feb26d1c346ee928fc550c369de3b97502
ILWID <Obasanyaolufemi@gmail.com>
2022-10-31 07:44:50 -0700

    Pointers

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _memcpy - function that copies memory area.
+ * @n: bytes from memory
+ * @src: source of size bytes
+ * @dest: memory destination
+ *
+ * Return: nothing
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	memcpy(dest, src, n);
+	return (0);
+}
